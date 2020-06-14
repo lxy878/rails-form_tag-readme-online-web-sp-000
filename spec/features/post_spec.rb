aac7a55e97049cb58f6d112383c75b6c5b7b0ffb
Xiaoyi Liu <lxy878@gmail.com>
2020-06-14 06:29:09 +0000

    Automatically backed up by Learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -7,13 +7,10 @@ describe 'new post' do
   end
 
   describe 'new post' do
-
-    ...
-
     it 'renders HTML in the /new template' do
       visit new_post_path
       expect(page).to have_content('Post Form')
     end
   end
-  
+
 end
