6ea328d6274d2b39fcb16b6be9a01b2be2d6e3d3
Xiaoyi Liu <lxy878@gmail.com>
2020-06-14 08:09:19 +0000

    Automatically backed up by Learn

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,6 +45,7 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.web_console.whitelisted_ips = '<IP address here>'
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
